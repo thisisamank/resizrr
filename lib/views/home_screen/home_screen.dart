96393c3f8f4699703c25e12870dd1e6b026de6c0
thisisamank <thisisamank@gmail.com>
2021-06-22 11:41:06 +0530

    bottom nav implemented

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -5,11 +5,49 @@ import 'package:resizrr/constants/colors.dart';
 import 'package:resizrr/view_models/select_image/image_view_model.dart';
 import 'package:resizrr/views/home_screen/working_image.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int imageMenuIndex = 0;
+
+  void _changeMenu(int index) {
+    setState(() => imageMenuIndex = index);
+  }
+
+  final List<Widget> _widgetOptions = [
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Business',
+    ),
+    Text(
+      'Index 2: School',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     final imageViewModel = Provider.of<SelectImageViewModel>(context);
+
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _changeMenu,
+        currentIndex: imageMenuIndex,
+        selectedItemColor: BrandColors.black,
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Edit Image',
+          ),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.equalizer), label: 'Image Filters'),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.maximize_outlined), label: 'Backgrounds'),
+        ],
+      ),
       appBar: AppBar(
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(
@@ -18,7 +56,7 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
               onPressed: () async => imageViewModel.saveimage(),
-              icon: const Icon(Icons.save))
+              icon: const Icon(Icons.file_download))
         ],
         elevation: 0,
       ),
@@ -26,6 +64,7 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             ShowImage(),
+            _widgetOptions.elementAt(imageMenuIndex),
           ],
         ),
       ),
